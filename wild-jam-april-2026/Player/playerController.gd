c74571eeba3f29e3a45a1f8e2e214ec107a03728
tempVincent <111691290+tempVincent@users.noreply.github.com>
2026-04-11 01:32:23 +0200

    Gegenstände
    Erweiterung:
    -Möbel
    -Caffe Kanne
    -Caffe Maschine

--- wild-jam-april-2026/Player/playerController.gd
+++ wild-jam-april-2026/Player/playerController.gd
@@ -3,6 +3,10 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+enum animationState {idle, idle2, walk, jump}
+var animationStatePlayer: animationState
+@onready var AnimationController =  $AnimatedSprite2D
+
 
 
 func _physics_process(delta: float) -> void:
@@ -21,5 +25,35 @@ func _physics_process(delta: float) -> void:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	
+	animation(direction)
+	
+	
+	
 	move_and_slide()
+
+
+func animation(direction) -> void:
+	if direction < 0:
+		AnimationController.flip_h = true
+	else:
+		AnimationController.flip_h = false
+
+	if Input.is_action_pressed("ui_left") or Input.is_action_pressed("ui_right"):
+		animationStatePlayer= animationState.walk
+	else:
+		animationStatePlayer= animationState.idle
+	
+	if Input.is_action_just_pressed("ui_accept"):
+		animationStatePlayer= animationState.jump
+	
+	
+	
+	####JUMP ANIMATION GEHT NOCH NICHT SAUBER
+	if AnimationController.animation != animationState.keys()[animationStatePlayer]:
+		AnimationController.play(animationState.keys()[animationStatePlayer])
+		
+		if animationStatePlayer == animationState.jump:
+			await AnimationController.animation_finished
+		
+	print(AnimationController.animation)
