8497083f26dbf43721a78d290b7569b9b0df8b9d
Fohlii <fohli54@gmail.com>
2026-04-11 21:37:40 +0200

    added timer

--- wild-jam-april-2026/time_button.gd
+++ wild-jam-april-2026/time_button.gd
@@ -1,12 +1,14 @@
 extends Node2D
-@onready var lineText:LineEdit = $LineEdit
+@onready var label:Label = $Label
 @onready var timer = $Timer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	#add method call for getting selected difficulty
 	#then set time left
-	lineText.text = "Time left: " + str(timer.time_left)
+	timer.wait_time = 300.0
+	timer.start()
+	label.text = "Time left: " + str(timer.time_left)
 	pass # Replace with function body.
 
 func _timer(difficulty):
@@ -14,4 +16,5 @@ func _timer(difficulty):
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	label.text = "Time left: " + str(int(timer.time_left))
+	
