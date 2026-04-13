d0d9be0cfa9d3d62d4f240263e9712a066d3eb23
Fohlii <fohli54@gmail.com>
2026-04-13 19:04:04 +0200

    idk

--- wild-jam-april-2026/Skripts/area_2d.gd
+++ wild-jam-april-2026/Skripts/area_2d.gd
@@ -11,13 +11,13 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-func _on_area_entered(body: Node2D):
-	if body.scene_file_path == "res://Objects/coffee_can.tscn":
+func _on_body_entered(body: Node2D):
+	if body.scene_file_path == "res://root/WorkInProgress/Objects/CoffeeCan":
 		inRangeObjects.append(body)
-		print("Coffe CAn")
+		print("I see: Coffe CAn")
 	
 	if body.scene_file_path == "res://Objects/coffeMachine.tscn":
 		inRangeObjects.append(body)
-		print("CoffeMachine")
+		print("i see: CoffeMachine")
 	else:
-		print("Unknown Object")
+		print(body.get_path())
