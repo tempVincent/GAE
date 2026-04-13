2185ab997f0d4bff38c692d4ca6352f6f863e12a
Fohlii <fohli54@gmail.com>
2026-04-13 19:23:51 +0200

    all objectives now should be completed if item dropped in front of johnny

--- wild-jam-april-2026/Skripts/area_2d.gd
+++ wild-jam-april-2026/Skripts/area_2d.gd
@@ -4,23 +4,24 @@ var inRangeObjects:Array
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
 func _on_body_entered(body: Node2D):
 	var path = body.get_path()
 	if str(path) == "/root/WorkInProgress/Objects/CoffeeCan":
 		if ObjectivePool.current_objective == ObjectivePool.objectives.coffee:
 			print("Objective completed")
 			ObjectivePool._complete_current_objective()
-		inRangeObjects.append(body)
 	
-	elif str(path) == "res://Objects/coffeMachine.tscn":
-		inRangeObjects.append(body)
+	elif str(path) == "/root/WorkInProgress/Objects/Fruits":
+		if ObjectivePool.current_objective == ObjectivePool.objectives.fruits:
+			print("Objective completed")
+			ObjectivePool._complete_current_objective()
+					
+	elif str(path) == "/root/WorkInProgress/Objects/Water":
+		if ObjectivePool.current_objective == ObjectivePool.objectives.water:
+			print("Objective completed")
+			ObjectivePool._complete_current_objective()
 		print("i see: CoffeMachine")
 	else:
 		print("Johhny:" + str(body.get_path()))
