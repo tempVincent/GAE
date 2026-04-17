ef1320844d8bc5676a4483e8dd3561ed35d205bf
Benedek Kovacs <kh233@stud.uni-heidelberg.de>
2026-04-17 20:50:46 +0200

    freeze dont work

--- wild-jam-april-2026/Classes/Fruit.gd
+++ wild-jam-april-2026/Classes/Fruit.gd
@@ -1,27 +1,36 @@
 class_name Fruit extends RigidBody2D
 
 var sprite: Sprite2D
+var area: Area2D
+var defaultTexture: CompressedTexture2D = preload("res://Assets/fruits/Banane.png")
+var peelTexture: CompressedTexture2D = preload("res://Assets/fruits/Chilli.png")
 var isInHand = false
 var characterHand
 var characterAnimation:AnimatedSprite2D
 @onready var collision:CollisionShape2D = $CollisionShape2D
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	# sprite = get_node("Sprite2D")
-	pass
+	sprite = get_node("Sprite2D")
+	area = get_node("Area2D")
+	sprite.texture = defaultTexture
+	area.area_entered.connect(becomeHazard)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
+func becomeHazard(a: Area2D) -> void:
+		print("fruit hit floor2")
+		get_parent().remove_child(self)
+		freeze = true
+		linear_velocity = Vector2(0, 0)
+		scale = Vector2(2, 2)
+		sprite.texture = peelTexture
+		set_collision_layer_value(13, true)
+		set_collision_layer_value(9, false)
+		set_collision_mask_value(9, false)
 
 func _enter_tree() -> void:
 	if get_parent().get_parent().get_parent().scene_file_path == "res://Player/silvester.tscn":
 		
 		##Get Resources
-		
 		characterHand = get_parent().get_node("AnimatedSprite2D/Hand")
 		characterAnimation = get_parent().get_parent()
 		
@@ -32,8 +41,6 @@ func _enter_tree() -> void:
 		collision.disabled = true
 		scale = Vector2(0.5,0.5)
 		rotation = 0
-		
-		pass
 
 func _exit_tree() -> void:
 	if get_parent().get_parent().get_parent().scene_file_path == "res://Player/silvester.tscn":
@@ -42,5 +49,4 @@ func _exit_tree() -> void:
 		isInHand = false
 		freeze = false
 		collision.disabled = false
-	
 		scale = Vector2(1,1)
