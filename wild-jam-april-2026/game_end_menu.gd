1f1bcd2effc94265b8184a2f439b740adefea723
tempVincent <111691290+tempVincent@users.noreply.github.com>
2026-04-18 23:40:40 +0200

    Fix 2

--- wild-jam-april-2026/game_end_menu.gd
+++ wild-jam-april-2026/game_end_menu.gd
@@ -3,8 +3,9 @@ extends TextureButton
 func _ready() -> void:
 	pass # Replace with function body.
 
-func _on_texture_button_pressed() -> void:
-	print("test")
+
+
+
+func _on_pressed() -> void:
 	get_tree().change_scene_to_file("res://WorkInProgress.tscn")
-	#et_tree().reload_current_scene()
 	pass # Replace with function body.
