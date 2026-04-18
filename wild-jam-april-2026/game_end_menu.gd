ac7b2c1c39f0e92eacdf6fd61b315d6d9034fac8
Fohlii <fohli54@gmail.com>
2026-04-18 23:31:03 +0200

    johnyy erkennt saft

--- wild-jam-april-2026/game_end_menu.gd
+++ wild-jam-april-2026/game_end_menu.gd
@@ -1,15 +1,11 @@
-extends Control
-
-
+extends TextureButton
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
-	
-func _show():
-	print("Show ")
-	self.visible = true
 
-func _on_texture_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://WorkInProgress.tscn")
-	#et_tree().reload_current_scene()
+func _on_pressed() -> void:
+	print("button klicked")
+	var err = get_tree().change_scene_to_file("res://WorkInProgress.tscn")
+	print("button klicked")
+	print("Scene change error: ", err)
 	pass # Replace with function body.
