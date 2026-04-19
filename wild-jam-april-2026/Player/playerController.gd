b9fe8e469691988c7ecfa27e066b43d34b3fa810
Fohlii <fohli54@gmail.com>
2026-04-19 18:34:20 +0200

    higher overal movement + press shift to sprint

--- wild-jam-april-2026/Player/playerController.gd
+++ wild-jam-april-2026/Player/playerController.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
+var SPEED = 400.0
 const JUMP_VELOCITY = -400.0
 enum animationState {idle, idle2, walk, jump}
 var animationStatePlayer: animationState
@@ -20,7 +20,10 @@ func _physics_process(delta: float) -> void:
 		# Handle jump.
 		if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 			velocity.y = JUMP_VELOCITY
-
+		if Input.is_action_pressed("run"):
+			SPEED = 550
+		if Input.is_action_just_released("run"):
+			SPEED = 400
 		# Get the input direction and handle the movement/deceleration.
 		# As good practice, you should replace UI actions with custom gameplay actions.
 		var direction := Input.get_axis("ui_left", "ui_right")
