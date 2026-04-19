c723c88706278450d6e29876f5af4c51b01237b3
Fohlii <fohli54@gmail.com>
2026-04-19 17:19:04 +0200

    end menu should play sound..

--- wild-jam-april-2026/game_end_menu.gd
+++ wild-jam-april-2026/game_end_menu.gd
@@ -1,8 +1,4 @@
 extends TextureButton
-@onready var player = $"../AudioStreamPlayer2"
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	player.playing = true
 
 func _on_pressed() -> void:
 	get_tree().change_scene_to_file("res://WorkInProgress.tscn")
