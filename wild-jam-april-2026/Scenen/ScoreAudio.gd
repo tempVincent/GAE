f783bb74c2976c5218bbf358a6684a53d8149746
Fohlii <fohli54@gmail.com>
2026-05-09 14:40:50 +0200

    small change: anpassung von success audio lautstärke

--- wild-jam-april-2026/Scenen/ScoreAudio.gd
+++ wild-jam-april-2026/Scenen/ScoreAudio.gd
@@ -6,7 +6,7 @@ extends AudioStreamPlayer
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var increaseVol:Tween = get_tree().create_tween()
-	increaseVol.tween_property(musicplayer,"volume_db",-80 + (settings.musicVol * 0.55),1.5).set_trans(Tween.TRANS_QUAD)
+	increaseVol.tween_property(musicplayer,"volume_db",-40 + (settings.musicVol * 0.55),1.5).set_trans(Tween.TRANS_QUAD)
 	await increaseVol.finished
 	increaseVol.kill()
 	pass # Replace with function body.
