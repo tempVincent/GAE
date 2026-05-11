2b36e6b849898f64f41467332561e957bd8f5c37
tempVincent <111691290+tempVincent@users.noreply.github.com>
2026-05-11 19:07:05 +0200

    Anpassung UI nach Lose

--- wild-jam-april-2026/Skripts/johnny.gd
+++ wild-jam-april-2026/Skripts/johnny.gd
@@ -2,6 +2,7 @@ extends Node2D
 @onready var gpu_particles_2d: GPUParticles2D = $GPUParticles2D2
 @onready var explosion_sound: AudioStreamPlayer2D = $ExplosionSound2
 @onready var jhonny: Node2D = $"."
+@onready var speachbubble = $"../UI/Speachbubble"
 
 func _ready():
 	ObjectivePool.player_lost.connect(_explode)
@@ -10,6 +11,7 @@ func _ready():
 func _explode():
 	print("Called")
 	explosion_sound.play()
+	speachbubble.visible = false
 	gpu_particles_2d.position = jhonny.global_position
 	gpu_particles_2d.restart()
 	gpu_particles_2d.emitting = true
